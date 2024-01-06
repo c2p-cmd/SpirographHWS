8ce4a004ab1655ba921a0a6d694cc95a18c49774
Sharan Thakur <sharandt19@live.com>
2024-01-06 13:24:45 +0530

    Initial Commit

---
+++ SpirographHWS/SpirographHWSApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpirographHWSApp.swift
+//  SpirographHWS
+//
+//  Created by Sharan Thakur on 06/01/24.
+//
+
+import SwiftUI
+
+@main
+struct SpirographHWSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
