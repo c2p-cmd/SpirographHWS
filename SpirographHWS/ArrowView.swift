f727489b4d47129513ebf0b398820f694c2fd717
Sharan Thakur <sharandt19@live.com>
2024-01-06 17:31:04 +0530

    Completed Challenge 2 and some decorations

--- SpirographHWS/ArrowView.swift
+++ SpirographHWS/ArrowView.swift
@@ -7,11 +7,19 @@
 
 import SwiftUI
 
-struct Arrow: Shape {
+// MARK: Arrow Insettable Shape
+struct Arrow: InsettableShape {
+    var insetAmount: CGFloat = 0.0
+    
+    var animatableData: CGFloat {
+        get { insetAmount }
+        set { insetAmount = newValue }
+    }
+    
     func path(in rect: CGRect) -> Path {
         var path = Path()
         
-        path.move(to: CGPoint(x: 0, y: rect.midY + rect.midY / 4))
+        path.move(to: CGPoint(x: rect.minX, y: rect.midY + rect.midY / 4))
         
         path.addLine(to: CGPoint(x: rect.maxX * 0.5, y: rect.midY + rect.midY / 4))
         path.addLine(to: CGPoint(x: rect.maxX * 0.5, y: rect.midY - rect.midY / 2))
@@ -21,45 +29,67 @@ struct Arrow: Shape {
         path.addLine(to: CGPoint(x: rect.maxX * 0.5, y: rect.midY + rect.midY / 2))
         path.addLine(to: CGPoint(x: rect.maxX * 0.5, y: rect.midY - rect.midY / 4))
         
-        path.addLine(to: CGPoint(x: 0, y: rect.midY - rect.midY / 4))
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.midY - rect.midY / 4))
         
-        path.addLine(to: CGPoint(x: 0, y: rect.midY + rect.midY / 4))
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.midY + rect.midY / 4))
         
         path.closeSubpath()
         return path
     }
+    
+    func inset(by amount: CGFloat) -> some InsettableShape {
+        var shape = self
+        shape.insetAmount += amount
+        return shape
+    }
 }
 
+// MARK: - Arrow's Host View
 struct ArrowView: View {
-    @State private var lineWidth = 10.0
+    @State private var insetAmount = 10.0
+    @State private var rotate = 0.0
     
     var body: some View {
         VStack {
-            Text("Create an Arrow shape – having it point straight up is fine. This could be a rectangle/triangle-style arrow, or perhaps three lines, or maybe something else depending on what kind of arrow you want to draw.")
-                .font(.system(.headline, design: .rounded, weight: .semibold))
-                .multilineTextAlignment(.leading)
-            
-            Spacer()
+            List {
+                Text("1. Create an Arrow shape – having it point straight up is fine. This could be a rectangle/triangle-style arrow, or perhaps three lines, or maybe something else depending on what kind of arrow you want to draw.")
+                
+                Text("2. Make the line thickness of your Arrow shape animatable.")
+            }
+            .scrollDisabled(true)
+            .listStyle(.grouped)
+            .font(.system(.headline, design: .rounded, weight: .semibold))
+            .multilineTextAlignment(.leading)
             
             Arrow()
-                .stroke(Color.yellow, lineWidth: lineWidth)
-                .fill(LinearGradient(
-                    colors: [.red, .purple, .blue],
-                    startPoint: .leading,
-                    endPoint: .trailing
-                ))
-                .animation(.bouncy, value: lineWidth)
+                .strokeBorder(
+                    Color.accent,
+                    style: StrokeStyle(lineWidth: insetAmount, lineCap: .round, lineJoin: .round)
+                )
+                .fill(.ultraThinMaterial)
+                .rotationEffect(.degrees(rotate), anchor: .center)
                 .frame(width: 200, height: 300)
-            
-            Spacer()
+                .padding(.bottom, 50)
+                .tabItem { Label("2", systemImage: "2.circle") }
+                .toolbar {
+                    ToolbarItem(placement: .bottomBar) {
+                        Button("Tap to Animate 😉", action: changeInset)
+                            .buttonStyle(.borderedProminent)
+                            .buttonBorderShape(.roundedRectangle(radius: 5))
+                    }
+                }
         }
-        .toolbar {
-            ToolbarItem(placement: .bottomBar) {
-                Stepper("LineWidth: \(lineWidth.formatted())", value: $lineWidth, in: 1...20, step: 0.5)
-            }
-        }
-        .padding(.horizontal)
         .navigationTitle("Arrow Challenge View")
+    }
+    
+    func changeInset() {
+        withAnimation(.bouncy(duration: 0.75, extraBounce: 0.5)) {
+            insetAmount = insetAmount > 10 ? 10 : 20
+        }
+        
+        withAnimation(.smooth(duration: 1, extraBounce: 0.5)) {
+            rotate -= 90
+        }
     }
 }
 
